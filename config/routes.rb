9138cd47bda8c73cbebe35ae0d0e0c76ab57782e
Juan Li Puma <juanlipuma94@gmail.com>
2016-12-21 20:32:55 -0300

    Style pages
    - Change homepage
    - Make Homepage and Show Persona pages pretty
    - Adapt HTML template code to HAML

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get 'persona/:id' => 'persona#show'
+  root 'home#index'
+
+  resources :persona, only: [:show]
 end

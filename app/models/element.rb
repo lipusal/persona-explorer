76b1dc9b846f11efab80ba418e625d40ea9dc702
Juan Li Puma <juanlipuma94@gmail.com>
2016-12-21 21:56:52 -0300

    Present some Persona stats
    - Add affinities table
    - Create basic elements table

--- app/models/element.rb
+++ app/models/element.rb
@@ -3,4 +3,12 @@ class Element < ApplicationRecord
 
   # has_many :persona_affinities
   # has_many :personas, through: :persona_affinities
+
+  #TODO Normalize elements
+
+  def self.basic
+    Element.all.reject do |e|
+      %w[All Not\ Applicable Recovery Bad\ Status Ailment].include? e.name
+    end
+  end
 end

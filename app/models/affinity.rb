b9fe7143d26d9caef4aeaea1ed43411775b79a19
Juan Li Puma <juanlipuma94@gmail.com>
2017-01-19 21:33:03 -0800

    Show prettier affinities
    - Add color and bold to affinities

--- app/models/affinity.rb
+++ app/models/affinity.rb
@@ -2,6 +2,27 @@
 class Affinity < ActiveRecord::Base
   belongs_to :persona
   belongs_to :element
+  validates_presence_of :effect
 
   delegate :name, to: :element
+
+  # Gets a pretty HTML and colored representation of this affinity. TODO do this in a presenter.
+  def present
+    color = case effect
+      when 'Absorbs', 'Reflects', 'Block'
+        'blue'
+      when 'Weak'
+        'red'
+      when 'Resists'
+        'green'
+      else
+        Rails.logger.warn "Unrecognized affinity effect #{effect}, defaulting to black color"
+        'black'
+    end
+    "<b style='color:#{color}'>#{effect}</b>"
+  end
+
+  def to_s
+    "#{persona.name}'s affinity to #{name}: #{effect}"
+  end
 end
